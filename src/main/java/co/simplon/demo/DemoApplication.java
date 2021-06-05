09a19e759b0bfe37d1e887c95dfa500915158c52
bernard hugueney <gitlab.com@bernard-hugueney.org>
2021-06-05 07:33:20 +0200

    JPA with persistence.xml

--- src/main/java/co/simplon/demo/DemoApplication.java
+++ src/main/java/co/simplon/demo/DemoApplication.java
@@ -1,4 +1,7 @@
 package co.simplon.demo;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,9 +17,31 @@ import java.util.Map;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		CityDaoJDBC testDao= new CityDaoJDBC();
-		System.out.println(testDao.findOne(10));
+        System.err.println("!!!!!");
+		testJPA();
+		System.err.println("!!!!!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	public static void testJDBC(){
+		CityDaoJDBC testDao= new CityDaoJDBC();
+		System.out.println(testDao.findOne(10));
+	}
+public static void testJPA(){
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa");
+	EntityManager entityManager = factory.createEntityManager();
+	co.simplon.demo.model.City city = entityManager.find(co.simplon.demo.model.City.class, 10L);
+	System.err.println(city);
+	co.simplon.demo.model.City city2= new co.simplon.demo.model.City("Atlantis", 0, 0.5);
+	System.out.println(city2);
+	entityManager.getTransaction().begin();
 
+	entityManager.persist(city2);
+	System.out.println(city2);
+
+	entityManager.getTransaction().commit();
+	System.out.println(city2);
+
+	entityManager.close();
+
+}
 }
